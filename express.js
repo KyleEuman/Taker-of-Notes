091f97857f1686b9e3092a1280f50455a7e87bec
Kyle Euman <euman_kyle@yahoo.com>
2021-04-10 10:55:54 -0400

    added paths to my html files

--- express.js
+++ express.js
@@ -5,3 +5,15 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/notes.html"));
+})
